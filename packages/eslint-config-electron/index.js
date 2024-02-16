9f7fbd854b1a1ee7ddbbea802147a8b587e14525
Trezy <tre@trezy.com>
2024-02-16 16:12:28 -0600

    feat: initial commit

---
+++ packages/eslint-config-electron/index.js
@@ -0,0 +1,14 @@
+export default {
+	env: {
+		browser: true,
+		es2022: true,
+		node: true,
+	},
+	extends: [
+		'plugin:import/electron',
+	],
+	globals: {
+		MAIN_WINDOW_VITE_DEV_SERVER_URL: 'readonly',
+		MAIN_WINDOW_VITE_NAME: 'readonly',
+	},
+}
---
+++ packages/eslint-config-react/index.js
@@ -0,0 +1,90 @@
+/* eslint-env node */
+
+module.exports = {
+	env: {
+		browser: true,
+		es2022: true,
+		node: true,
+	},
+	extends: [
+		'plugin:jsx-a11y/recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+		'plugin:react-perf/recommended',
+		'plugin:react-prefer-function-component/recommended',
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	plugins: [
+		'react',
+		'react-prefer-function-component',
+	],
+	rules: {
+		// react
+		'react/boolean-prop-naming': ['error'],
+		'react/default-props-match-prop-types': ['error'],
+		'react/destructuring-assignment': ['error'],
+		'react/forbid-elements': ['error', {
+			forbid: [
+				{
+					element: 'a',
+					message: 'Use <Link> component instead.',
+				},
+				{
+					element: 'button',
+					message: 'Use <Button> component instead.',
+				},
+			],
+		}],
+		'react/jsx-boolean-value': ['error'],
+		'react/jsx-closing-bracket-location': ['error', 'after-props'],
+		'react/jsx-curly-brace-presence': ['error', 'always'],
+		'react/jsx-filename-extension': ['error'],
+		'react/jsx-first-prop-new-line': ['error', 'multiline'],
+		'react/jsx-handler-names': ['error'],
+		'react/jsx-indent': ['error', 'tab'],
+		'react/jsx-indent-props': ['error', 'tab'],
+		'react/jsx-key': ['error'],
+		'react/jsx-max-props-per-line': ['error'],
+		'react/jsx-no-script-url': ['error'],
+		'react/jsx-no-useless-fragment': ['error'],
+		'react/jsx-pascal-case': ['error'],
+		'react/jsx-props-no-multi-spaces': ['error'],
+		'react/jsx-sort-props': ['error', {
+			reservedFirst: true,
+		}],
+		'react/jsx-wrap-multilines': ['error', {
+			declaration: 'parens-new-line',
+			assignment: 'parens-new-line',
+			return: 'parens-new-line',
+			arrow: 'parens-new-line',
+			condition: 'parens-new-line',
+			logical: 'parens-new-line',
+			prop: 'parens-new-line',
+		}],
+		'react/no-danger': ['error'],
+		'react/no-invalid-html-attribute': ['error'],
+		'react/no-typos': ['error'],
+		'react/no-unused-prop-types': ['error'],
+		'react/prefer-stateless-function': ['error'],
+		'react/require-default-props': ['error'],
+		'react/self-closing-comp': ['error'],
+		'react/sort-prop-types': ['error'],
+		'react/style-prop-object': ['error'],
+		'react/void-dom-elements-no-children': ['error'],
+
+		// react-prefer-function-component
+		'react-prefer-function-component/react-prefer-function-component': ['error', {
+			allowComponentDidCatch: false,
+		}],
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+}
