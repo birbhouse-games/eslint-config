9f212cdf758e44f33d6218ee53e06c4b0a5f2869
Trezy <tre@trezy.com>
2024-05-20 13:24:01 -0500

    chore: update to support ESLint 9
    BREAKING CHANGE: Flat configs won't work with ESLint 8 or below.

--- packages/eslint-config-electron/index.js
+++ packages/eslint-config-electron/index.js
@@ -1,16 +1,19 @@
 /* eslint-env node */
 
-module.exports = {
-	env: {
-		browser: true,
-		es2022: true,
-		node: true,
+/** @type {import('eslint').ESLint.ConfigData[]} */
+export default [
+	{
+		env: {
+			browser: true,
+			es2022: true,
+			node: true,
+		},
+		extends: [
+			'plugin:import/electron',
+		],
+		globals: {
+			MAIN_WINDOW_VITE_DEV_SERVER_URL: 'readonly',
+			MAIN_WINDOW_VITE_NAME: 'readonly',
+		},
 	},
-	extends: [
-		'plugin:import/electron',
-	],
-	globals: {
-		MAIN_WINDOW_VITE_DEV_SERVER_URL: 'readonly',
-		MAIN_WINDOW_VITE_NAME: 'readonly',
-	},
-}
+]
